127dfcaa1a436339ece7bde39121db3e666f6e4a
Alex Lenkoski <alex@nr.no>
2017-02-14 15:27:58 +0100

    Adding logic for function source hashing

--- R/package.management.R
+++ R/package.management.R
@@ -1,3 +1,29 @@
+#' Get the hash of an entire namespace
+#' @export
+hash.namespace <- function(pkg.name)
+{
+  ##------- Load the library -----------
+  library(pkg.name, character.only=TRUE)
+  ##------------------------------------
+  
+  ##------ Get namespace --------------
+  fs = ls(as.environment(paste0("package:",pkg.name)))
+  ##-----------------------------------
+
+  ##------- Hash ---------
+  h.all <- list()
+  for(i in 1:length(fs))
+  {
+    str.f <- fs[i]
+    f.str <- eval(parse(text = str.f))
+    h.all[[str.f]] <- hash.obj(f.str)
+  }
+  ##----------------------
+
+  return(h.all)
+}
+
+
 #' Compare the output of two benchmarks
 benchmark.compare.output <- function(pkg.name,branch.orig, branch.new,output.loc, verbose)
 {
@@ -51,7 +77,8 @@ benchmark.run.comparison <- function(pkg.loc="./",
                                      branch.new = "dev",
                                      output.loc = "/tmp/",
                                      verbose=TRUE,
-                                     datasets=NULL)
+                                     datasets=NULL,
+                                     differences.only=TRUE)
 {
 
   ##------ First check that environment is clean and committed -----
@@ -61,13 +88,34 @@ benchmark.run.comparison <- function(pkg.loc="./",
   if(!is.null(attr(a.1,"status")) | !is.null(attr(a.2,"status")) ) stop("Your develop environment is not committed, unable to switch between branches")
   ##----------------------------------------------------------------
 
+  ##------ Hash namespace first? -----------------------------------
+  if(differences.only)
+  {
+    for(branch in c(branch.orig, branch.new))
+    {
+      ##-------- Install and precalculate hashes --------------------------
+      command <- paste(branch,pkg.loc,pkg.name,sep="','")
+      system2("Rscript", paste0("-e \"NRTools::git.install.branch('",command,"')\"") )
+      command <- paste(pkg.name, branch, output.loc,sep="','")
+      system2("Rscript", paste("-e \"NRTools::hash.namespace('",command, "')\""))
+      ##-------------------------------------------------------------------
+    }
 
+    fs <- benchmark.compare.hashed.branches(pkg.name,branch.orig, branch.new,output.loc)
+    if(fs == FALSE) return(TRUE)
+  }else{
+    fs <- NULL
+  }
+  ##----------------------------------------------------------------
+
+
+  
   for(b in c(branch.orig, branch.new))
   {
     ##-------- Install and save the original benchmarks ----------------
     command <- paste(b,pkg.loc,pkg.name,sep="','")
     system2("Rscript", paste0("-e \"NRTools::git.install.branch('",command,"')\"") )
-    command <- paste(pkg.loc,pkg.name, b, output.loc, verbose, datasets,sep="','")
+    command <- paste(pkg.loc,pkg.name, b, output.loc, verbose, datasets,fs,sep="','")
     system2("Rscript", paste("-e \"NRTools::benchmark.generate.comparison('",command, "')\""))
     ##-------------------------------------------------------------------
   }
@@ -80,6 +128,43 @@ benchmark.run.comparison <- function(pkg.loc="./",
   
 }
 
+benchmark.calculate.hashes <- function(pkg.name,
+                                       branch="master"
+                                       output.loc="/tmp/")
+{
+  h.all <- hash.namespace(pkg.name)
+  save(h.all, file = paste0(output.loc,"/hash_",pkg.name,"_",branch,".RData"))
+}
+  
+benchmark.compare.hashed.branches <- function(pkg.name,
+                                              branch.orig = "master",
+                                              branch.new = "dev",
+                                              output.loc = "/tmp/")
+{
+  load(paste0(output.loc, "/hash_",pkg.name,"_",branch.orig,".RData"))
+  h.orig <- h.all
+  load(paste0(output.loc, "/hash_",pkg.name,"_",branch.new,".RData"))
+  h.new <- h.all
+  names.new <- names(h.new)
+  names.orig <- names(h.orig)
+  w.in.both <- which(names.orig %in% names.new)
+  if(length(w.in.both))return(FALSE)
+  fs.changed <- NULL
+  for(j in 1:length(w.in.both))
+  {
+    f <- names.orig[ w.in.both[j] ]
+    if(h.orig[[f]] != h.new[[f]])
+    {
+      fs.changed <- c(fs.changed, f)
+    }
+  }
+  if(length(fs.changed) == 0)
+  {
+    return(FALSE)
+  }else{
+    return(fs.changed)
+  }
+}
 
 #' Write benchmarks out for a comparison branch
 #' @description This function goes through the entire namespace of a given package, tests if there are benchmarks in the examples code .  If there is a benchmark, the code for that benchmark, and the reulting output are saved.
@@ -92,7 +177,8 @@ benchmark.generate.comparison <- function(pkg.loc="./",
                                           branch = "master",
                                           output.loc = "/tmp/",
                                           verbose=TRUE,
-                                          datasets=NULL)
+                                          datasets=NULL,
+                                          fs = NULL)
 {
 
   ##------- Load the library -----------
@@ -100,7 +186,10 @@ benchmark.generate.comparison <- function(pkg.loc="./",
   ##------------------------------------
   
   ##------ Get namespace --------------
-  fs = ls(as.environment(paste0("package:",pkg.name)))
+  if(is.null(fs))
+  {
+    fs = ls(as.environment(paste0("package:",pkg.name)))
+  }
   ##-----------------------------------
   
   ##-------- Setup directories ------------------
